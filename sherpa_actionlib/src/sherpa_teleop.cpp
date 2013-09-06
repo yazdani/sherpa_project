40e67ad2c7275801b869e389cfb44574215b3946
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2013-09-06 15:26:35 +0200

    added animation into joystick-teleop

--- sherpa_actionlib/src/sherpa_teleop.cpp
+++ sherpa_actionlib/src/sherpa_teleop.cpp
@@ -1,5 +1,4 @@
 
-
 #include "ros/ros.h"
 #include <sensor_msgs/Joy.h>
 #include "geometry_msgs/Twist.h"
@@ -11,13 +10,15 @@
 #include <math.h>
 #include <fcntl.h>
 ros::Time last_recieved_joy_message_time_;
+//using gazebo;
 class TeleopMoving
 {
   public:
    TeleopMoving();
   private:
   ros::NodeHandle nh_;
-
+  ros::NodeHandle nh1_;
+  // SherpaHumanPlugin shp;
   int linear_, angular_;
   double l_scale_, a_scale_;
   double run_button;
@@ -29,34 +30,20 @@ class TeleopMoving
    void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 };
 
-  TeleopMoving::TeleopMoving():
-    linear_(1),
-    angular_(2)
+  TeleopMoving::TeleopMoving()
   {  
-
-  ROS_INFO("setting of the default");
-  
-  nh_.param("axis_linear", linear_, linear_);
-  nh_.param("axis_angular", angular_, angular_);
-  nh_.param("scale_linear", a_scale_, a_scale_);
-  nh_.param("scale_angular", l_scale_, l_scale_);
- nh_.param("run_button", run_button, run_button);
-
-  
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-  //vel_pub1_ = nh_.advertise<std_msgs::String>("cmd_vel1", 1);
-  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopMoving::joyCallback, this);
-  ROS_INFO("finish of the default");
-}
+    vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+      vel_pub1_ = nh1_.advertise<std_msgs::String>("cmd_pose", 1);
+    joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopMoving::joyCallback, this);
+  }
 
   //~TeleopMoving() {}
 
   void TeleopMoving::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-   ROS_INFO("setting of the joycall");
   last_recieved_joy_message_time_ = ros::Time::now();
- geometry_msgs::Twist twister;
- // std_msgs::String str;
+  geometry_msgs::Twist twister;
+  std_msgs::String poser;
  //pressing the two sticks
 
  bool l1_stick = ((0 < joy->axes.size()) &&  joy->axes[0]); //LEFT R&L
@@ -69,13 +56,7 @@ class TeleopMoving
  bool axes_up = ((2 < joy->buttons.size()) && joy->axes[2]);  
   bool axes_down = ((3 < joy->buttons.size()) && joy->axes[3]);
   */
- ROS_INFO("\n");
- ROS_INFO("l1_stick: %d", l1_stick);
- ROS_INFO("l2_stick: %d", l2_stick);
- ROS_INFO("r1_stick: %d", r1_stick);
- ROS_INFO("r2_stick: %d", r2_stick);
- ROS_INFO("\n");
- ROS_INFO("twister-dirty %d", twister.dirty);
+ // ROS_INFO("twister-dirty %d", twister.dirty);
  //pressing of the several buttons
   bool button_A = ((0 < joy->buttons.size()) && joy->buttons[0]);
   bool button_B = ((1 < joy->buttons.size()) && joy->buttons[1]);
@@ -85,58 +66,46 @@ class TeleopMoving
   bool button_RB = ((5 < joy->buttons.size()) && joy->buttons[5]);
  
 
-  ROS_INFO("\n");
-  ROS_INFO("button_A %d", button_A);
-  ROS_INFO("button_B %d", button_B);
-  ROS_INFO("button_X %d", button_X);
-  ROS_INFO("button_Y %d", button_Y);
-  ROS_INFO("button_LB %d", button_LB);
-  ROS_INFO("button_RB %d", button_RB);  
-  ROS_INFO("\n");
 
-  if(l1_stick == true && button_LB == false)
+  if(l1_stick == true && button_LB == false && button_Y == false)
     { //go ahead
     twister.linear.x = 1; 
-    ROS_INFO("twister.x %f\n", twister.linear.x);
-  }else if(l1_stick == true && button_LB == true)
+    poser.data="walk"; 
+  }else if(l1_stick == true && button_LB == true && button_Y == false)
     { //go back
-    twister.linear.x = -1; 
-    ROS_INFO("twister.x %f\n", twister.linear.x);
-  }else  if(r1_stick == true && button_RB == false)
+    twister.linear.x = -1;
+    poser.data="walk"; 
+  }else  if(r1_stick == true && button_RB == false && button_Y == false)
     { //go left
-    twister.linear.y = -1; 
-    ROS_INFO("twister.linear.y %f\n", twister.linear.y);
-  }else if(r1_stick == true && button_RB == true)
+    twister.linear.y = -1;
+    poser.data="walk"; 
+ }else if(r1_stick == true && button_RB == true && button_Y == false)
     { //go right
-    twister.linear.y = 1; 
-    ROS_INFO("twister.linear.y %f\n", twister.linear.y);
-    }else if(button_X == true)
+    twister.linear.y = 1;
+    poser.data="walk"; 
+   }else if(button_X == true)
     { //rotate left
       twister.angular.z = 1;
-      ROS_INFO("twister.angular.z %f\n", twister.angular.z);
     }else if(button_B == true)
     { //rotate right
       twister.angular.z = -1;
-       ROS_INFO("twister.angular.z %f\n", twister.angular.z);
+    }else if(button_Y == true )
+    {
+      poser.data="point";
     }
  
  
  
   
- ROS_INFO("setting of the joycall");
- ROS_INFO("twister.x %f", twister.linear.x);
- ROS_INFO("twister.y %f", twister.linear.y);
- ROS_INFO("twister.z %f", twister.linear.z);
- vel_pub_.publish(twister);
+ vel_pub_.publish(twister); 
+  vel_pub1_.publish(poser);
+
  //vel_pub1_.publish(str);
 }
 
 int main(int argcs, char** argv)
 {
   ros::init(argcs, argv, "teleop_sherpa_human");
-  ROS_INFO("HELLLOO");
    TeleopMoving teleop_moving;
-
-   ROS_INFO("HI");
   ros::spin();
 }
