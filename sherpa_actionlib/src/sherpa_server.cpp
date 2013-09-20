e0e1bcbbd5f5044e4c4a5cebc61e959e30691a5a
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2013-09-20 18:07:49 +0200

    actionlibrary completed!

--- sherpa_actionlib/src/sherpa_server.cpp
+++ sherpa_actionlib/src/sherpa_server.cpp
@@ -7,6 +7,8 @@
 #include <sstream>
 #include <string>
 #include <iostream>
+#include <tf/LinearMath/Quaternion.h>
+
 using namespace std;
 class trajAction
 {
@@ -14,25 +16,22 @@ protected:
 
   ros::NodeHandle nh_;
   ros::NodeHandle nh2_; 
- // NodeHandle instance must be created before this line. Otherwise strange error may occur.
   actionlib::SimpleActionServer<sherpa_actionlib::trajAction> as_; 
   std::string action_name_;
-  // create messages that are used to published feedback/result
   sherpa_actionlib::trajFeedback feedback_;
   sherpa_actionlib::trajResult result_;
   ros::ServiceClient gms_c; 
   gazebo_msgs::GetModelState getmodelstate; 
-  float db;
   gazebo_msgs::SetModelState setmodelstate;
   gazebo_msgs::ModelState modelstate;
-
+  
 public:
   
   trajAction(std::string name) :
-    as_(nh_, name, boost::bind(&trajAction::executeCB, this, _1), false),
+  as_(nh_, name, boost::bind(&trajAction::executeCB, this, _1), false),
   action_name_(name)
   {
-
+    
     as_.start();
     ROS_INFO("Waiting for action client to start...");
   }
@@ -41,15 +40,6 @@ public:
   {
   }
   
-  string inverter(float val)
-  {
-   ostringstream Str;
-    db = val;
-    Str << db;
-    string digit(Str.str());
-     return digit;
-  }
- 
   void executeCB(const sherpa_actionlib::trajGoalConstPtr &goal)
   {
     ROS_INFO("Action client registered, starting executing!");
@@ -58,51 +48,79 @@ public:
     getmodelstate.request.model_name="Sherpa_Human";
     // helper variables
     ros::Rate r(1);
+ 
     bool success = true;
-    
-    gms_c.call(getmodelstate);
-    // publish info to the console for the user
+    geometry_msgs::Twist tw;
+    geometry_msgs::Pose ps;
+    float vectordistance = 1.0;
     ROS_INFO("%s: Executing!", action_name_.c_str());
-    //feedback_.feed ="Not ready! Is still working...";
+      
+    ps.position.x = getmodelstate.response.pose.position.x;
+    ps.position.y = getmodelstate.response.pose.position.y;     
+    ps.position.z = getmodelstate.response.pose.position.z;
+    ps.orientation.x = getmodelstate.response.pose.orientation.x;
+    ps.orientation.y = getmodelstate.response.pose.orientation.y;
+    ps.orientation.z = getmodelstate.response.pose.orientation.z;
+   
+    tw.linear.x = getmodelstate.response.twist.linear.x;
+    tw.linear.y = getmodelstate.response.twist.linear.y;     
+    tw.linear.z = getmodelstate.response.twist.linear.z;
+    tw.angular.x = getmodelstate.response.twist.angular.x;
+    tw.angular.y = getmodelstate.response.twist.angular.y;
+    tw.angular.z = getmodelstate.response.twist.angular.z;
+
     
-    if(feedback_.pose.size()==0)
+    float pythagoras = sqrt((
+			   ((goal->setPoseGoalBusyGenius.position.x - ps.position.x)*(goal->setPoseGoalBusyGenius.position.x - ps.position.x))
+			   + ((goal->setPoseGoalBusyGenius.position.y - ps.position.y)*(goal->setPoseGoalBusyGenius.position.y - ps.position.y)) 
+			   +  ((goal->setPoseGoalBusyGenius.position.z - ps.position.z)*(goal->setPoseGoalBusyGenius.position.z - ps.position.z))));
+
+    while(pythagoras > vectordistance)
       {
-    feedback_.pose.push_back("pose: ");
-    feedback_.pose.push_back(" position: ");
-    feedback_.pose.push_back("  x: " + inverter(getmodelstate.response.pose.position.x));
-    feedback_.pose.push_back("  y: " + inverter(getmodelstate.response.pose.position.y));     
-    feedback_.pose.push_back("  z: "+ inverter(getmodelstate.response.pose.position.z));     
-    feedback_.pose.push_back(" orientation");
-    feedback_.pose.push_back("  x: "+ inverter(getmodelstate.response.pose.orientation.x));     
-    feedback_.pose.push_back("  y: "+inverter(getmodelstate.response.pose.orientation.y));     
-    feedback_.pose.push_back("  z: "+inverter(getmodelstate.response.pose.orientation.z));
-    feedback_.pose.push_back("twist: ");
-    feedback_.pose.push_back(" linear: ");
-    feedback_.pose.push_back("  x: "+inverter(getmodelstate.response.twist.linear.x));     
-    feedback_.pose.push_back("  y: "+inverter(getmodelstate.response.twist.linear.y));     
-    feedback_.pose.push_back("  z: "+inverter(getmodelstate.response.twist.linear.z));
-    feedback_.pose.push_back(" angular: ");
-    feedback_.pose.push_back("  x: "+inverter(getmodelstate.response.twist.angular.x));
-    feedback_.pose.push_back("  y: "+inverter(getmodelstate.response.twist.angular.y));
-    feedback_.pose.push_back("  z: "+inverter(getmodelstate.response.twist.angular.z));
-      }
-    //feedback_.feed ="Executing is finished!";
-    if (as_.isPreemptRequested() || !ros::ok())
-      {
-        ROS_INFO("%s: Preempted", action_name_.c_str());
-        // set the action state to preempted
-        as_.setPreempted();
-        success = false;
+	if(as_.isPreemptRequested() || !ros::ok())
+	  {
+	    ROS_INFO("%s: Preempted", action_name_.c_str());
+	    // set the action state to preempted
+	    as_.setPreempted();
+	    success = false;
+	    break;
+	  }
+	gms_c.call(getmodelstate);
+	
+	ps.position.x = getmodelstate.response.pose.position.x;
+	ps.position.y = getmodelstate.response.pose.position.y;     
+	ps.position.z = getmodelstate.response.pose.position.z;
+	ps.orientation.x = getmodelstate.response.pose.orientation.x;
+	ps.orientation.y = getmodelstate.response.pose.orientation.y;
+	ps.orientation.z = getmodelstate.response.pose.orientation.z;
+	
+	tw.linear.x = getmodelstate.response.twist.linear.x;
+	tw.linear.y = getmodelstate.response.twist.linear.y;     
+	tw.linear.z = getmodelstate.response.twist.linear.z;
+	tw.angular.x = getmodelstate.response.twist.angular.x;
+	tw.angular.y = getmodelstate.response.twist.angular.y;
+	tw.angular.z = getmodelstate.response.twist.angular.z;
+        ROS_INFO("%f pythagoras", pythagoras);
+	  pythagoras = sqrt((
+			     ((goal->setPoseGoalBusyGenius.position.x - ps.position.x)*(goal->setPoseGoalBusyGenius.position.x - ps.position.x))
+			     + ((goal->setPoseGoalBusyGenius.position.y - ps.position.y)*(goal->setPoseGoalBusyGenius.position.y - ps.position.y)) 
+			     +  ((goal->setPoseGoalBusyGenius.position.z - ps.position.z)*(goal->setPoseGoalBusyGenius.position.z - ps.position.z))));
+	  
+	  feedback_.getPoseOfBusyGenius = ps;
+	  feedback_.getTwistOfBusyGenius= tw;
+	  as_.publishFeedback(feedback_);
+	  r.sleep();
       }
     // publish the feedback
-    as_.publishFeedback(feedback_);
-    // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-    r.sleep();
     
+    // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
+ 
     
     if(success)
       {
-	result_.res = "Executing is finished";//feedback_.sequence[1];
+	result_.end = "Executing is finished";//feedback_.sequence[1];
+        result_.getTwist = tw;
+        result_.getPose = ps;
       }	
     ROS_INFO("%s: Succeeded", action_name_.c_str());
     // set the action state to succeeded
