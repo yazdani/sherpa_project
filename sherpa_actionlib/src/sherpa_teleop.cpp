de6dd9e02ea59086f4d413f5816416e6a0af4296
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2013-10-04 14:19:47 +0200

    updated the teleop for sherpa_human

--- sherpa_actionlib/src/sherpa_teleop.cpp
+++ sherpa_actionlib/src/sherpa_teleop.cpp
@@ -32,6 +32,12 @@ class TeleopMoving
 
   TeleopMoving::TeleopMoving()
   {  
+ ROS_INFO("Starting the Keyboard:");
+  std::cout << "Pushing the Buttons\n" << std::endl;
+  std::cout << "Y : Pointing out\n" << std::endl;
+ std::cout << "X : Left Rotation\n" << std::endl;
+ std::cout << "B : Right Rotation\n" << std::endl;
+ std::cout << "LB & RB : Going backwards \n" << std::endl;
     vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
       vel_pub1_ = nh1_.advertise<std_msgs::String>("cmd_pose", 1);
     joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopMoving::joyCallback, this);
@@ -41,6 +47,7 @@ class TeleopMoving
 
   void TeleopMoving::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
+ 
   last_recieved_joy_message_time_ = ros::Time::now();
   geometry_msgs::Twist twister;
   std_msgs::String poser;
@@ -56,8 +63,10 @@ class TeleopMoving
  bool axes_up = ((2 < joy->buttons.size()) && joy->axes[2]);  
   bool axes_down = ((3 < joy->buttons.size()) && joy->axes[3]);
   */
+
  // ROS_INFO("twister-dirty %d", twister.dirty);
  //pressing of the several buttons
+
   bool button_A = ((0 < joy->buttons.size()) && joy->buttons[0]);
   bool button_B = ((1 < joy->buttons.size()) && joy->buttons[1]);
   bool button_X = ((2 < joy->buttons.size()) && joy->buttons[2]);
