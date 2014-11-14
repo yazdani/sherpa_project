d1cfd1c8db40b4c20320b76f977dd1a8fc442d1b
Yazdani <yazdani@cs.uni-bremen.de>
2014-11-14 18:33:29 +0100

    added stuff

---
+++ agents_model_description/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
+
+export CATKIN_TEST_RESULTS_DIR="/home/yazdani/work/ros/hydro/rosbuild_ws/workspace/sherpa_project/agents_model_description/build/test_results"
+export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
